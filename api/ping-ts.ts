e49605c3d805ddc12321c5e6c7cd0b8938ba9102
tedovelchev08-oss <tedo.velchev08@gmail.com>
2026-09-04 09:50:50 +0300

    Update ping-ts.ts

--- api/ping-ts.ts
+++ api/ping-ts.ts
@@ -1,3 +1,6 @@
-export default function handler(req: any, res: any) {
+export default function handler(
+  _req: unknown,
+  res: { status: (n: number) => { json: (b: unknown) => void } },
+) {
   res.status(200).json({ ok: true, lang: "ts" });
 }
